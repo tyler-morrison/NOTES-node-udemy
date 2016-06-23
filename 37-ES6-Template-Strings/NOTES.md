2ba15370db142107d8259c78d09fc8c8194196ca
tyler-morrison <tyler.morrison@thehartford.com>
2016-06-23 14:46:23 -0400

    Finished 37

---
+++ 37-ES6-Template-Strings/NOTES.md
@@ -0,0 +1,3 @@
+# Node, ES6, and Template Strings
+
+**Template Literal** = A way to concatenate strings in Javascript (ES6). Easier to work with than a bunch of strings concatenated with ‘+’.
---
+++ 37-ES6-Template-Strings/app.js
@@ -0,0 +1,10 @@
+// Old method of concatenation...
+var name = 'John Doe';
+
+var greet = 'Hello ' + name;
+
+// ES6 template literal style...
+var greet2 = `Hello ${ name }`;
+
+console.log(greet);
+console.log(greet2);
