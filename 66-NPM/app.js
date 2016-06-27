0eeff3d2dbf6390f9072c2188dd811c5c298f1b0
Tyler Morrison <tyler@counterpointstudio.com>
2016-06-27 13:37:47 -0400

    Finished 71

--- 66-NPM/app.js
+++ 66-NPM/app.js
@@ -1,3 +1,30 @@
-const moment = require('moment');
+'use strict';
 
-console.log(moment().format("ddd, hA"));
+const http = require('http');
+const fs = require('fs');
+
+http.createServer((req, res) => {
+
+    switch (req.url) {
+        case '/':
+            fs.createReadStream(__dirname + '/index.html').pipe(res);
+
+            break;
+        case '/api':
+            res.writeHead(200, {
+                'Content-Type': 'application/json'
+            });
+
+            let obj = {
+                firstname: 'Jane',
+                lastname: 'Doe'
+            };
+            res.end(JSON.stringify(obj));
+
+            break;
+        default:
+            res.writeHead(404);
+            res.end();
+    }
+
+}).listen(1337, '127.0.0.1');
